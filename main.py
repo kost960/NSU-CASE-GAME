a84e7b6af3802b4e605a200f41702c53a8d3ab25
kost960 <pozitq3@gmail.com>
2024-04-03 00:04:25 +0700

    Add players count

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from random import randint
 
 
+def players():
+    players_count = int(input('Введите количество игроков: '))
+    players = {}
+    for i in range(players_count):
+        p = Player()
+        players[i + 1] = p
+
+
 class Player:
     population = 100
-    economy = 10
-    military_power = 0
-    science_and_technology = 0
-    resources = 0
-    territory = 0
+    economy = 100
+    military_power = 100
+    science_and_technology = 100
+    resources = 100
+    territory = 100
+
+    def stats(self):
+        return (f"""Население вашего государства: {self.population}
+Ваша экономика: {self.economy}
+Военная мощь государства: {self.military_power}
+Наука и технологии: {self.science_and_technology}
+Ваши ресурсы: {self.resources}
+Ваши территории: {self.territory}""")
+
 
 p = Player()
 
@@ -19,51 +36,40 @@ class Enemy:
 
 def menu(p):
     while True:
-        print("""1) Сражаться
+        print(f"""
+Меню:
+1) Сражаться
 2) Статистика
 3) Посадить зерно""")
-        try:
-            n = input("Введите число: ")
+        n = input("Введите число: ")
 
-            if int(n) == 1:
-                menu_fight(p)
-            elif int(n) == 2:
-                menu_stats(p)
-            elif int(n) == 3:
-                eat(p)
-            else:
-                print("Чего ждем?")
-
-        except NameError:
-            ...
-        except SyntaxError:
-            ...
-        except ValueError:
-            ...
+        if int(n) == 1:
+            menu_fight(p)
+        elif int(n) == 2:
+            menu_stats(p)
+        elif int(n) == 3:
+            eat(p)
+        else:
+            print("Чего ждем?")
 
 
 def menu_stats(p):
     print("Статистика игрока")
     print("*****************")
-    print(f"""
-Население вашего государства: {p.population}
-Ваша экономика: {p.economy}
-Военная мощь государства: {p.military_power}
-Наука и технологии: {p.science_and_technology}
-Ваши ресурсы: {p.political_stability}
-Ваши территории: {p.territory}
-""")
+    print(p.stats())
+
 
 def eat(p):
     print("Посадить зерно")
     p.resources += randint(1, 5) * randint(1, 5)
     p.territory -= 10
 
+
 def menu_fight(p):
     p.hp = 100
     e = Enemy()
 
-    print(f"Вы hp: {p.hp} damage: {p.damage}")
+    print(f"Вы hp: {p.population} damage: {p.military_power}")
     print(f"Враг hp: {e.hp} damage: {e.damage}")
     print("**********************")
     while e.hp > 0:
@@ -72,30 +78,28 @@ def menu_fight(p):
         n = input("Введите число: ")
         if int(n) == 1:
 
-            e.hp -= p.damage
+            e.hp -= p.military_power
             print(f"Вы ударили противника, у него осталось {e.hp} hp")
-            p.hp -= e.damage
-            print(f"Противник ударил вас, у вас осталось {p.hp} hp")
+            p.population -= e.damage
+            print(f"Противник ударил вас, у вас осталось {p.population} hp")
 
             print("*********************")
 
         elif int(n) == 2:
 
-            p.hp += randint(0, 5)
-            if p.hp > 100:
-                p.hp = 100
+            p.population += randint(0, 5)
+            if p.population > 100:
+                p.population = 100
 
-            print(f"Ваши хп {p.hp}")
+            print(f"Ваши хп {p.population}")
 
         else:
             print("Чего ждем?")
-        if p.hp < 0:
+        if p.population < 0:
             print("Вы проиграли")
-            p.fol += 1
             break
         if e.hp < 0:
             print("Вы победили")
-            p.wins += 1
             break
 
 
