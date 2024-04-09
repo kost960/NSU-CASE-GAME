1e98d0418f4ede3127a0e8b289dc218cf655634e
Predlct <p.redict@mail.ru>
2024-04-09 21:32:06 +0700

    edit

--- player_actions.py
+++ player_actions.py
@@ -15,7 +15,10 @@ def eat(player):
         i = int(input('Какую часть территории вы собираетесь выделить?\n'))
         player.resources += randint(1, 5) * 5 * (i//10)
         player.territory -= (i//10)*10
+    elif number == 2:
+
     else:
+        print(f'Wrong, try again') and eat(player)
 
 
 def science(player):
@@ -23,9 +26,15 @@ def science(player):
                        f'1)Да\n'
                        f'2)Нет\n'))
     if number == 1:
+        if player.resources < 25:
+
         player.resources -= 25
         player.science_and_technology += 1
+    elif number == 2:
+
     else:
+        print(f'Wrong, try again') and science(player)
+
 
 def military(player):
     number = int(input(f'Использовать 25 ед ресурсов для повышения военной мощности государства на 1 единицу?\n'
@@ -34,6 +43,10 @@ def military(player):
     if number == 1:
         player.resources -= 25
         player.military_power += 1
+    elif number == 2:
+
+    else:
+        print(f'Wrong, try again') and military(player)
 
 
 def territory(player):
@@ -43,10 +56,29 @@ def territory(player):
     if number != 2:
         player.population -= (randint(0,10)/10)*number
         player.territory += randint(5,15)*number
+    elif number == 2:
+
+
+def reform(player):
+    number = int(input(f'Вы можете провести масштабные реформы в сферах: экономики, социальной '
+                      f'и государственного регулирования.\n'
+                      f'Стоимость: 350 единиц ресурсов, 100 единиц территории.\n'
+                      f'Повышает экономику государства на 1 балл.\n'
+                      f'1)Провести\n'
+                      f'2)Не проводить\n'))
+    if number == 1:
+        if player.resources < 350 or player.territory < 101:
+            print('Недостаточно ресурсов, попробуйте в другой раз.') and reform(player)
+        else:
+            player.resources -= 350
+            player.territory -= 100
+            player.economy += 1
+    elif number == 2:
+
     else:
+        print(f'Wrong, try again') and reform(player)
 
 
-def
 
 actions = {
     '0':menu_stats,
@@ -54,5 +86,5 @@ actions = {
     '2':science,
     '3':military,
     '4':territory,
-    '5':
+    '5':reform
     }
